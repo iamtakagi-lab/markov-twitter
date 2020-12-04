11c3660603b250cc336d2a6aa5435b72ee1bc63f
riptakagi <smatchjp@gmail.com>
2020-12-04 17:40:37 +0900

    feat: Stream reply

--- src/main.py
+++ src/main.py
@@ -13,6 +13,12 @@ from flask import Flask, request, redirect, abort, jsonify
 
 from flask_apscheduler import APScheduler
 
+import datetime
+
+import markovify
+
+import logging
+
 class Config(object):
     SCHEDULER_API_ENABLED = True
 
@@ -21,12 +27,9 @@ scheduler = APScheduler()
 
 app = Flask(__name__)
 
-import datetime
-
-import markovify
-
-import logging
 logging.basicConfig(level=logging.DEBUG)
+
+screen_name = os.environ["SCREEN_NAME"]
 
 # Twitter API Keys
 twitterKeys = {"CK": os.environ["TWITTER_API_CONKEY"],
@@ -38,20 +41,69 @@ twitterKeys = {"CK": os.environ["TWITTER_API_CONKEY"],
 # MeCab
 mec = MeCab.Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd -O wakati")
 
+# Twitter
+twt = TwitterTools(
+        twitterKeys["CK"], twitterKeys["CS"], twitterKeys["AT"], twitterKeys["ATS"])
+
 @app.route('/')
 def index():
     return "Markov Twitter"
 
 
-# TLからツイートを学習して呟きます (30分おき) 
+def reply(status):
+    global screen_name, twt
+
+    user_screen_name = status["user"]["screen_name"]
+
+    # 自身のツイートには反応しない
+    if user_screen_name == screen_name: return
+
+    # 文書生成
+    if not os.path.isfile("./chainfiles/home_timeline.json"):
+        return print('Learned model file not found. まずはじめにツイートを学習させてください。')
+
+    startWith = ""
+    length = ""
+    try:
+        with open("./chainfiles/home_timeline.json") as f:
+            textModel = markovify.Text.from_json(f.read())
+            if startWith and 0 < len(startWith.strip()):
+                startWithStr = mec.parse(startWith).strip().split()
+                if textModel.state_size < len(startWithStr):
+                    startWithStr = startWithStr[0:textModel.state_size]
+                startWithStr = " ".join(startWithStr)
+                try:
+                    sentence = textModel.make_sentence_with_start(
+                    startWithStr, tries=100)
+                except KeyError:
+                    return print('生成失敗。該当開始語が存在しません。')
+            elif str(length).isdecimal():
+                sentence = textModel.make_short_sentence(
+                    int(length), tries=100)
+            else:
+                sentence = textModel.make_sentence(tries=100)
+            if sentence is not None:
+                sentence = "".join(sentence.split())
+
+            params = {
+                "status": "@" + user_screen_name + " " + sentence,
+                "in_reply_to_status_id": status["id_str"],
+            }
+
+            # 返信
+            twt.postTweet(params)
+
+    except Exception as e:
+        print(e)
+
+
+# TLからツイートを学習して呟きます (30分おき)
 scheduler.task('cron', id='tweet', minute='*/30')
 # @scheduler.task('interval', id='tweet', seconds=30, misfire_grace_time=900) # DEBUG
 def tweet():
-    global twitterKeys
-    twt = TwitterTools(
-        twitterKeys["CK"], twitterKeys["CS"], twitterKeys["AT"], twitterKeys["ATS"])
+    global twt
 
-    # 1. TLから呟きを学習
+    # TLから呟きを学習
     try:
         params = {}
         filepath = os.path.join("./chainfiles", "home_timeline.json")
@@ -61,7 +113,7 @@ def tweet():
     except Exception as e:
         print(e)
 
-    # 2. 文書生成
+    # 文書生成
     if not os.path.isfile("./chainfiles/home_timeline.json"):
         return print('Learned model file not found. まずはじめにツイートを学習させてください。')
 
@@ -92,7 +144,7 @@ def tweet():
                     "status": sentence
                 }
 
-                # 3. 呟く
+                # 呟く
                 twt.postTweet(params)
             else:
                 print('生成失敗。複数回試してみてください。')
@@ -101,6 +153,13 @@ def tweet():
 
 
 if __name__ == "__main__":
+
+    # Tweet Scheduler
     scheduler.init_app(app)
     scheduler.start()
+
+    # Stremaming
+    twt.startStreaming({'track':'@' + screen_name}, reply)
+
+    # Start
     app.run(host = os.getenv('HOST', '0.0.0.0'), port = int(os.getenv('PORT', '5000')))
