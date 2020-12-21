e47dfb2dfb3c1a428b76165569265246552d74f6
riptakagi <smatchjp@gmail.com>
2020-12-21 18:13:24 +0900

    feat: only apscheduler with run on heroku worker container.

--- src/main.py
+++ src/main.py
@@ -9,9 +9,12 @@ import urllib.parse
 import os
 import sys
 
-from flask import Flask, request, redirect, abort, jsonify
+# from flask import Flask, request, redirect, abort, jsonify
 
-from flask_apscheduler import APScheduler
+# from flask_apscheduler import APScheduler
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
 
 import datetime
 
@@ -23,9 +26,7 @@ class Config(object):
     SCHEDULER_API_ENABLED = True
 
 
-scheduler = APScheduler()
-
-app = Flask(__name__)
+#app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -45,10 +46,11 @@ mec = MeCab.Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd -O wakati")
 twt = TwitterTools(
         twitterKeys["CK"], twitterKeys["CS"], twitterKeys["AT"], twitterKeys["ATS"])
 
+"""
 @app.route('/')
 def index():
     return "Markov Twitter"
-
+"""
 
 def reply(status):
     global screen_name, twt
@@ -98,8 +100,9 @@ def reply(status):
 
 
 # TLからツイートを学習して呟きます (30分おき)
-scheduler.task('cron', id='tweet', minute='*/30')
+# @scheduler.task('cron', id='tweet', minute='*/30')
 # @scheduler.task('interval', id='tweet', seconds=30, misfire_grace_time=900) # DEBUG
+@sched.scheduled_job('cron', id='tweet', minutes='*/30')
 def tweet():
     global twt
 
@@ -155,11 +158,15 @@ def tweet():
 if __name__ == "__main__":
 
     # Tweet Scheduler
-    scheduler.init_app(app)
-    scheduler.start()
+    # scheduler.init_app(app)
+    # scheduler.start()
+
+    # scheduler.add_job(tweet, 'interval', minutes='*/30')
+
+    sched.start()
 
     # Stremaming
-    twt.startStreaming({'track':'@' + screen_name}, reply)
+    # twt.startStreaming({'track':'@' + screen_name}, reply)
 
     # Start
-    app.run(host = os.getenv('HOST', '0.0.0.0'), port = int(os.getenv('PORT', '5000')))
+    # app.run(host = os.getenv('HOST', '0.0.0.0'), port = int(os.getenv('PORT', '5000')))
